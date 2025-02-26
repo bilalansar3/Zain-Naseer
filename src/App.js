6c26154e078b31c5396df98df9c458555538342e
bilalansar3 <doctorofseoandcodding@gmail.com>
2025-02-26 11:02:12 +0500

    create a folder zain name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main';
-
+import Zain from './components/Zain'
 function App() {
   return (
     <div className="App">
 <Main/>
+<Zain/>
     </div>
   );
 }
